f1f6912e0f13b7f70039979c2cf7ff850324b83c
Hadiuzzaman1983 <hadi.personal1983@gmail.com>
2025-09-27 08:39:13 +0600

    first commit

---
+++ store/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from category.models import Category
+
+
+# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    price = models.IntegerField()
+    images= models.ImageField(upload_to="photos/products", null=True, blank=True)
+    stock = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
+
+
